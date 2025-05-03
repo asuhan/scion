a270578cb2de97230c4ca64be652e8b344e4756f
Alex Șuhan <alex.suhan@gmail.com>
2025-05-03 20:31:21 +0300

    Add skeleton text iterators

---
+++ Source/WebCore/platform_scion/graphics/SurrogatePairAwareTextIterator.swift
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) Research In Motion Limited 2011. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+
+struct SurrogatePairAwareTextIterator {
+  // The passed in UChar pointer starts at 'currentIndex'. The iterator operates on the range [currentIndex, lastIndex].
+  // 'endIndex' denotes the maximum length of the UChar array, which might exceed 'lastIndex'.
+  init(characters: CharSpanWrapper<UChar>, currentIndex: UInt32, lastIndex: UInt32) {
+    // TODO(asuhan): implement this
+    fatalError("Not implemented")
+  }
+
+  func consume(character: inout UInt32, clusterLength: inout UInt32) -> Bool {
+    // TODO(asuhan): implement this
+    fatalError("Not implemented")
+  }
+
+  func advance(advanceLength: UInt32) {
+    // TODO(asuhan): implement this
+    fatalError("Not implemented")
+  }
+}
