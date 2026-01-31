888fa29286e1296271c3e099d160eaeb6d213dbb
Alex Șuhan <alex.suhan@gmail.com>
2026-01-31 16:23:53 +0200

    Implement layoutBlock in render deprecated flexible box

--- Source/WebCore/rendering_scion/RenderDeprecatedFlexibleBox.swift
+++ Source/WebCore/rendering_scion/RenderDeprecatedFlexibleBox.swift
@@ -41,6 +41,23 @@ private func childDoesNotAffectWidthOrFlexing(_ child: RenderObjectWrapper) -> B
   return child.isOutOfFlowPositioned()
 }
 
+// TODO(asuhan): use an inline capacity of 8
+typealias ChildFrameRects = [LayoutRectWrapper]
+
+private func appendChildFrameRects(
+  _ box: RenderDeprecatedFlexibleBoxWrapper?, _ childFrameRects: inout ChildFrameRects
+) {
+  // TODO(asuhan): implement this
+  fatalError("Not implemented")
+}
+
+private func repaintChildrenDuringLayoutIfMoved(
+  _ box: RenderDeprecatedFlexibleBoxWrapper?, _ oldChildRects: ChildFrameRects
+) {
+  // TODO(asuhan): implement this
+  fatalError("Not implemented")
+}
+
 final class RenderDeprecatedFlexibleBoxWrapper: RenderBlockWrapper {
   override func styleWillChange(diff: StyleDifference, newStyle: RenderStyleWrapper) {
     let shouldClearLineClamp = { [self] () in
@@ -62,6 +79,98 @@ final class RenderDeprecatedFlexibleBoxWrapper: RenderBlockWrapper {
   override func layoutBlock(
     relayoutChildren: Bool, pageLogicalHeight: LayoutUnit = LayoutUnit(value: UInt64(0))
   ) {
+    assert(needsLayout())
+
+    if !relayoutChildren && simplifiedLayout() {
+      return
+    }
+
+    var relayoutChildren = relayoutChildren
+    let repainter = LayoutRepainter(renderer: self)
+    do {
+      let _ = LayoutStateMaintainer(
+        root: self, offset: locationOffset(),
+        disablePaintOffsetCache: isTransformed() || hasReflection()
+          || style().isFlippedBlocksWritingMode())
+
+      resetLogicalHeightBeforeLayoutIfNeeded()
+      preparePaginationBeforeBlockLayout(relayoutChildren: &relayoutChildren)
+
+      let previousSize = size()
+
+      updateLogicalWidth()
+      updateLogicalHeight()
+
+      if previousSize != size()
+        || (parent()!.isRenderDeprecatedFlexibleBox()
+          && parent()!.style().boxOrient() == .Horizontal
+          && parent()!.style().boxAlign() == .Stretch)
+      {
+        relayoutChildren = true
+      }
+
+      setHeight(height: 0)
+
+      stretchingChildren = false
+
+      let oldLayoutDelta = view().frameView().layoutContext().layoutDelta()
+
+      // Fieldsets need to find their legend and position it inside the border of the object.
+      // The legend then gets skipped during normal layout. The same is true for ruby text.
+      // It doesn't get included in the normal layout process but is instead skipped.
+      layoutExcludedChildren(relayoutChildren: relayoutChildren)
+
+      var oldChildRects = ChildFrameRects()
+      appendChildFrameRects(self, &oldChildRects)
+
+      if isHorizontal() {
+        layoutHorizontalBox(relayoutChildren)
+      } else {
+        layoutVerticalBox(relayoutChildren)
+      }
+
+      repaintChildrenDuringLayoutIfMoved(self, oldChildRects)
+      assert(view().frameView().layoutContext().layoutDeltaMatches(delta: oldLayoutDelta))
+
+      let oldClientAfterEdge = clientLogicalBottom()
+      updateLogicalHeight()
+
+      if previousSize.height() != height() {
+        relayoutChildren = true
+      }
+
+      layoutPositionedObjects(relayoutChildren: relayoutChildren || isDocumentElementRenderer())
+
+      updateDescendantTransformsAfterLayout()
+
+      computeOverflow(oldClientAfterEdge: oldClientAfterEdge)
+    }
+
+    updateLayerTransform()
+
+    if let layoutState = view().frameView().layoutContext().layoutState(),
+      layoutState.pageLogicalHeight().bool()
+    {
+      setPageLogicalOffset(
+        logicalOffset: layoutState.pageLogicalOffset(child: self, childLogicalOffset: logicalTop()))
+    }
+
+    // Update our scrollbars if we're overflow:auto/scroll/hidden now that we know if
+    // we overflow or not.
+    updateScrollInfoAfterLayout()
+
+    // Repaint with our new bounds if they are different from our old bounds.
+    repainter.repaintAfterLayout()
+
+    clearNeedsLayout()
+  }
+
+  private func layoutHorizontalBox(_ relayoutChildren: Bool) {
+    // TODO(asuhan): implement this
+    fatalError("Not implemented")
+  }
+
+  private func layoutVerticalBox(_ relayoutChildren: Bool) {
     // TODO(asuhan): implement this
     fatalError("Not implemented")
   }
@@ -162,8 +271,15 @@ final class RenderDeprecatedFlexibleBoxWrapper: RenderBlockWrapper {
     fatalError("Not implemented")
   }
 
+  private func isHorizontal() -> Bool {
+    // TODO(asuhan): implement this
+    fatalError("Not implemented")
+  }
+
   private func clearLineClamp() {
     // TODO(asuhan): implement this
     fatalError("Not implemented")
   }
+
+  private var stretchingChildren = false
 }
