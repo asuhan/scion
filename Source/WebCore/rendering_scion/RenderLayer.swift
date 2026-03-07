62bd159ddc2a9f306f97ff9af3ab3eee311d07ca
Alex Șuhan <alex.suhan@gmail.com>
2026-03-07 15:19:31 +0200

    Implement hasDescendantNeedingUpdateBackingOrHierarchyTraversal in layer

--- Source/WebCore/rendering_scion/RenderLayer.swift
+++ Source/WebCore/rendering_scion/RenderLayer.swift
@@ -771,8 +771,8 @@ class RenderLayerWrapper {
   }
 
   func hasDescendantNeedingUpdateBackingOrHierarchyTraversal() -> Bool {
-    // TODO(asuhan): implement this
-    fatalError("Not implemented")
+    assert(isNative)
+    return compositingDirtyBits.contains(.HasDescendantNeedingBackingOrHierarchyTraversal)
   }
 
   func needsCompositingPaintOrderChildrenUpdate() -> Bool {
