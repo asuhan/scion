cd0c69db8f8413ef134df3b3c624830cba8bb661
Alex Șuhan <alex.suhan@gmail.com>
2026-03-18 16:38:07 +0200

    Implement printRect in render view

--- Source/WebCore/rendering_scion/RenderView.swift
+++ Source/WebCore/rendering_scion/RenderView.swift
@@ -552,8 +552,8 @@ class RenderViewWrapper: RenderBlockFlowWrapper {
   }
 
   func printRect() -> IntRect {
-    // TODO(asuhan): implement this
-    fatalError("Not implemented")
+    assert(isNativeImpl())
+    return m_legacyPrinting.m_printRect
   }
 
   func setIsInWindow(_ isInWindow: Bool) {
