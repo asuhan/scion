01d2e334b9ceefb99c23871eb1a59bc4cc91520e
Alex Șuhan <alex.suhan@gmail.com>
2026-09-19 16:56:22 +0300

    Add absoluteQuadsForBox to render fragmented flow

--- Source/WebCore/rendering_scion/RenderFragmentedFlow.swift
+++ Source/WebCore/rendering_scion/RenderFragmentedFlow.swift
@@ -194,6 +194,17 @@ class RenderFragmentedFlowWrapper: RenderBlockFlowWrapper {
   }
 
   // Called when a descendant box's layout is finished and it has been positioned within its container.
+  func absoluteQuadsForBox(
+    _ quads: inout [FloatQuad], _ wasFixed: inout Bool?, _ box: RenderBoxWrapper
+  ) -> Bool {
+    if !hasValidFragmentInfo() {
+      return false
+    }
+
+    // TODO(asuhan): implement this
+    fatalError("Not implemented")
+  }
+
   func fragmentedFlowDescendantBoxLaidOut(descendant: RenderBoxWrapper) {}
 
   override func styleDidChange(diff: StyleDifference, oldStyle: RenderStyleWrapper?) {
